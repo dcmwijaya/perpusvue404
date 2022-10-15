22ff414b5493f5cbaf9769615f9218b7c8477565
devancakra <mudrawijaya@gmail.com>
2022-10-15 21:13:31 +0700

    Add: First

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg=""/>
 </template>
 
 <script>
@@ -20,7 +20,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
